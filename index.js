171b562fd694595af1ae8a482ea25e1f424c7a14
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-02-23 13:55:17 -0700

    complete

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ 
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((r) => r.json())
+    .then((data) => {
+      renderBooks(data);
+    });
 }
 
 function renderBooks(books) {
